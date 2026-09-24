0db06aece3c056dc4f17adce00be06b34886df3d
jspusa <jaydenhsu@petlifeco.com>
2026-09-24 21:16:00 +0800

    Avoid fighting messenger auto-scroll in regression

--- scripts/test-ui-upgrade.py
+++ scripts/test-ui-upgrade.py
@@ -56,8 +56,6 @@ def geometry(page, name):
         assert button['bottom'] <= min(result['dialog']['bottom'], result['height']) + 1, result
         assert button['left'] >= 0 and button['right'] <= result['width'] + 1, result
     area = page.locator('.secretary-chat')
-    area.evaluate('(el)=>{el.scrollTop=0}')
-    assert area.evaluate('(el)=>el.scrollTop') == 0
     area.evaluate('(el)=>{el.scrollTop=el.scrollHeight}')
     if area.evaluate('(el)=>el.scrollHeight>el.clientHeight'):
         assert area.evaluate('(el)=>el.scrollTop') > 0, result
