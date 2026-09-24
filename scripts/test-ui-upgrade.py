57340cc8e7f29aebac3af250f843f34b61f0b8e7
jspusa <jaydenhsu@petlifeco.com>
2026-09-24 13:43:22 +0800

    Run V4.6 regression suite against V5.2 shell

--- scripts/test-ui-upgrade.py
+++ scripts/test-ui-upgrade.py
@@ -26,7 +26,7 @@ base = args.url or 'http://127.0.0.1:8765/'
 
 def reach_meals(page):
     page.goto(base, wait_until='networkidle')
-    expect(page.locator('.app-version')).to_have_text('V4.6')
+    expect(page.locator('.app-version')).to_have_text('V5.2')
     expect(page.locator('#exportPdfBtn')).to_be_disabled()
     page.locator('#secretaryBtn').click()
     for value in ['胡志明市', '2026/10/14 09:40', '2026/10/17 17:30']:
